d3889af75c27e51d85b947e9337be06e8a069f5c
yuujis <king_7642@yahoo.co.jp>
2021-08-04 14:28:31 +0000

    Change from HTTPS to SSH

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,9 +1,9 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
-  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
+  before_action :admin_or_correct_user, only: [:index,:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
-  
+
   
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
@@ -31,17 +31,26 @@ class AttendancesController < ApplicationController
   end
 
   def update_one_month
+     
     ActiveRecord::Base.transaction do # トランザクションを開始します。
       attendances_params.each do |id, item|
+        if item[:started_at].blank? && item[:finished_at].present?
+          flash[:danger] = "出勤時間の入力データがない為、更新をキャンセルしました。"
+          redirect_to attendances_edit_one_month_user_url(date: params[:date]) and return
+        elsif item[:started_at].present? && item[:finished_at].blank?
+          flash[:danger] = "退勤時間の入力データがない為、更新をキャンセルしました。"
+          redirect_to attendances_edit_one_month_user_url(date: params[:date]) and return
+        end
         attendance = Attendance.find(id)
         attendance.update_attributes!(item)
       end
     end
     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
     redirect_to user_url(date: params[:date])
+    
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
-    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+    redirect_to attendances_edit_one_month_user_url(date: params[:date]) and return
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :correct_user, only: [:edit, :update, :show, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:index,:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
+   
   
   def index
     @users = User.paginate(page: params[:page]).search(params[:search])
